3b3c60072d43d7cdd94c71423a52f5fd892deb39
AerinLom <aerinlom1@gmail.com>
2024-05-23 06:56:37 -0700

    Task 4: Stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,19 @@ def status():
     """
     resp = {"status": "OK"}
     return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrieves the number of objects by type.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
