61829ecd26df8f49df262ab7b49a4ab687f376ba
MKDLAMINI <dlaminimkultra@gmail.com>
2024-05-24 07:00:38 -0700

    Task 7, somehow struggling to get the POST method to work, please have a look at it and see where I have implemented the function incorrectly

---
+++ api/v1/views/cities.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+"""
+route for handling State objects and operations
+"""
+from flask import jsonify, abort, request
+from models.state import State
+from api.v1.views import app_views, storage
+from models.city import City
+
+
+@app_views.route("/states/<state_id>/cities", methods=["GET"],
+                 strict_slashes=False)
+def city_by_state(state_id):
+    """
+    retrieves all City objects from a specific state
+    :return: json of all cities in a state or 404 on error
+    """
+    state = storage.get(State, state_id)
+    if not state:
+        return abort(404)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>',
+                 strict_slashes=False)
+def get_city(state_id):
+    """
+    gets a city
+    """
+    city = storage.get(City, city_id)
+    if city:
+        return jsonify(city.to_dict())
+    else:
+        return abort(404)
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """
+    deletes a city
+    """
+    city = storage.get(City, city_id)
+    if city:
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        return abort(404)
+
+
+@app_views.route('/states/<state_id>/cities',  methods=['POST'], strict_slashes=False)
+def create_city(city_id):
+    """
+    creates a city
+    """
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
+    state = storage.get(State, state_id)
+    if not state:
+        return abort(404)
+    if not request.get_json():
+        return abort(400, 'Not a JSON')
+    data = request.get_json()
+    if 'name' not in data:
+        return abort(400, 'Missing name')
+    data['state_id'] = state_id
+
+    city = City(**data)
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route("/cities/<city_id>",  methods=["PUT"],
+                 strict_slashes=False)
+def city_update(city_id):
+    """
+    update city by id
+    """
+    if request.content_type != 'application/json':
+        return abort(400, 'Not a JSON')
+    city = storage.get(City, city_id)
+    if city:
+        if not request.get_json():
+            return abort(400, 'Not a JSON')
+
+        data = request.get_json()
+        ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(city, key, value)
+        city.save()
+        return jsonify(city.to_dict()), 200
+    else:
+        return abort(404)
+
+    
